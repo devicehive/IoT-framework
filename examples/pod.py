59af3df173c2a14e86c2a41bea163f1116351309
astaff <artyom.astafurov@gmail.com>
2015-04-28 08:00:25 -0400

    Sample scripts for BLE Pulse Oxymeter and Scales

---
+++ examples/pod.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import dbus
+from dbus.mainloop.glib import DBusGMainLoop
+from gi.repository import GObject
+import array
+import time
+
+DBusGMainLoop(set_as_default=True)
+
+def get_ble():
+    return dbus.Interface(dbus.SystemBus().get_object("com.devicehive.bluetooth", '/com/devicehive/bluetooth'), "com.devicehive.bluetooth")
+
+ble = get_ble()
+def device_discovered(mac, name, rssi):
+    print("Discovered %s" % (name)) 
+    if (name == 'POD'):
+        ble.ScanStop()
+        time.sleep(5)
+        ble.Connect(mac)
+
+def device_connected(mac):
+    print("Connected to %s" % (mac))    
+    try:
+        ble.GattWrite(mac, "ffb2", "aa550f038401e0")
+        ble.GattNotifications(mac, "ffb2", True)
+
+    except dbus.DBusException as e:
+        print(e)
+
+def notification_received(mac, uuid, message):
+    print("MAC: %s, UUID: %s, Received: %s" % (mac, uuid, message))
+
+def main():
+    ble.connect_to_signal("DeviceDiscovered", device_discovered)
+    ble.connect_to_signal("DeviceConnected", device_connected)
+    ble.connect_to_signal("NotificationReceived", notification_received)
+
+    ble.ScanStart()
+
+    GObject.MainLoop().run()
+
+if __name__ == '__main__':
+    main()
---
+++ examples/scales.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import dbus
+from dbus.mainloop.glib import DBusGMainLoop
+from gi.repository import GObject
+import array
+import time
+
+DBusGMainLoop(set_as_default=True)
+
+def get_ble():
+    return dbus.Interface(dbus.SystemBus().get_object("com.devicehive.bluetooth", '/com/devicehive/bluetooth'), "com.devicehive.bluetooth")
+
+ble = get_ble()
+def device_discovered(mac, name, rssi):
+    print("Discovered %s %s" % (name, mac)) 
+    if (mac == 'e4c95cc8bbc1'):
+        print("Connecting")
+        ble.ScanStop()
+        ble.Connect(mac)
+        time.sleep(10)
+        ble.ScanStart()
+
+def device_connected(mac):
+    print("Connected to %s" % (mac))    
+    try:
+        ble.GattIndications(mac, "299d64102f6111e281c10800200c9a66", True)
+
+    except dbus.DBusException as e:
+        print(e)
+
+def indication_received(mac, uuid, message):
+    print("MAC: %s, UUID: %s, Received: %s" % (mac, uuid, message))
+
+def main():
+    ble.connect_to_signal("DeviceDiscovered", device_discovered)
+    ble.connect_to_signal("DeviceConnected", device_connected)
+    ble.connect_to_signal("IndiacationReceived", indication_received)
+
+    ble.ScanStart()
+
+    GObject.MainLoop().run()
+
+if __name__ == '__main__':
+    main()
