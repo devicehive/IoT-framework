51f237d56f529d722fa0b0cb08e2493207e29cea
mibori <miborishante@gmail.com>
2015-05-08 18:14:49 +0300

    godbus helpers draft/partially

--- dbushelp/example-consts.go
+++
@@ -1,9 +0,0 @@
-package dbushelp
-
-const (
-	ExamplePath_ComDevicehiveCloud  = "/com/devicehive/cloud"
-	ExampleIface_ComDevicehiveCloud = "com.devicehive.cloud"
-
-	ExamplePath_ComDevicehiveBluetooth  = "/com/devicehive/bluetooth"
-	ExampleIface_ComDevicehiveBluetooth = "com.devicehive.bluetooth"
-)
