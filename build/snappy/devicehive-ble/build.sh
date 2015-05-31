d114f2699183dd46ff69e9db65b4216f24910751
Sergey Demyanov <s.demyanov@gmail.com>
2015-05-31 07:23:57 -0700

    fixed build dependencies

--- build/snappy/devicehive-ble/build.sh
+++ build/snappy/devicehive-ble/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 go get github.com/devicehive/gatt
+go get github.com/godbus/dbus
 GOOS=linux go build -o bin/x86_64/devicehive-ble ../../../devicehive-ble/devicehive-ble.go
 GOOS=linux GOARCH=arm GOARM=7 go build -o bin/armhf/devicehive-ble ../../../devicehive-ble/devicehive-ble.go
