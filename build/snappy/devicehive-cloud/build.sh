1e85cf3dd752dc4520e86a89f2d2bb52a5e72f7b
Sergey Demyanov <s.demyanov@gmail.com>
2015-05-31 13:21:04 -0700

    relative path workaround

--- build/snappy/devicehive-cloud/build.sh
+++ build/snappy/devicehive-cloud/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+DIR="$(dirname "$(readlink -f "$0")")"
+
 go get github.com/godbus/dbus
-GOOS=linux go build -o bin/x86_64/devicehive-cloud ../../../devicehive-cloud/devicehive-cloud.go ../../../devicehive-cloud/rest-serve.go ../../../devicehive-cloud/ws-serve.go
-GOOS=linux GOARCH=arm GOARM=7 go build -o bin/armhf/devicehive-cloud ../../../devicehive-cloud/devicehive-cloud.go ../../../devicehive-cloud/rest-serve.go ../../../devicehive-cloud/ws-serve.go
+GOOS=linux go build -o $DIR/bin/x86_64/devicehive-cloud $DIR/../../../devicehive-cloud/devicehive-cloud.go $DIR/../../../devicehive-cloud/rest-serve.go $DIR/../../../devicehive-cloud/ws-serve.go
+GOOS=linux GOARCH=arm GOARM=7 go build -o $DIR/bin/armhf/devicehive-cloud $DIR/../../../devicehive-cloud/devicehive-cloud.go $DIR/../../../devicehive-cloud/rest-serve.go $DIR/../../../devicehive-cloud/ws-serve.go
