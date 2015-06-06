8589ed4e84ff75e13598efa961acfafa0b6c84c8
astaff <artyom.astafurov@gmail.com>
2015-06-05 22:42:38 -0400

    Added alljoyn thin client interop. Haven't tested yet

--- devicehive-alljoyn/alljoyn-bridge.go
+++ devicehive-alljoyn/alljoyn-bridge.go
@@ -7,108 +7,192 @@ package main
 // #include <aj_guid.h>
 // #include <aj_creds.h>
 // #include "alljoyn.h"
+//
+// AJ_Object Create_AJ_Object(char* path, AJ_InterfaceDescription* interfaces, uint8_t flags, void* context) {
+//   AJ_Object obj = {path, interfaces, flags, context};
+//   return obj;
+// }
+//
+//
 import "C"
 import (
 	"github.com/godbus/dbus"
 	"github.com/godbus/dbus/introspect"
 	"log"
+	"unsafe"
 )
 
 type AllJoynBridge struct {
-	bus *dbus.Conn
+	bus      *dbus.Conn
 	services map[string][]*introspect.Node
 }
-
-type 
 
 func NewAllJoynBridge(bus *dbus.Conn) *AllJoynBridge {
 	bridge := new(AllJoynBridge)
 	bridge.bus = bus
-	services = make(map[string][]*introspect.Node)
+	bridge.services = make(map[string][]*introspect.Node)
 
 	return bridge
 }
 
-func InitAllJoyn() {
-	C.AJ_Initialize()
-}
-
-
 func ParseArgumentOrProperty(name, access, _type string) string {
-	argString := name
-	if (access == "in" || access == "write") {
-		argString = argString + "<"
-	} else if (access == "out" || access == "read") {
-		argString = argString + ">"
-	} else if {
-		argString = argString + "="
+	s := name
+	if access == "in" || access == "write" {
+		s = s + "<"
+	} else if access == "out" || access == "read" {
+		s = s + ">"
+	} else {
+		s = s + "="
 	}
-	argString = argString + _type
+	s = s + _type
 
-	return argString
+	return s
 }
 
 func ParseArguments(args []introspect.Arg) string {
 	argString := ""
-	for arg := range(args) {
+	for _, arg := range args {
 		argString = argString + " " + ParseArgumentOrProperty(arg.Name, arg.Direction, arg.Type)
 	}
 	return argString
 }
 
-func ParseAllJoynInterfaces(interfaces []introspect.Interfaces) []AJ_InterfaceDescription {
-	res := make([]AJ_InterfaceDescription)
+func ParseAllJoynInterfaces(interfaces []introspect.Interface) []C.AJ_InterfaceDescription {
+	res := make([]C.AJ_InterfaceDescription, 1)
 
-	for iface := range(interfaces) {
-		desc := make([]string)
-		desc := append(desc, iface.Name)
+	for _, iface := range interfaces {
+		desc := make([]*C.char, 0)
+		desc = append(desc, C.CString(iface.Name))
 
-		for method := range(iface.Methods) {
+		for _, method := range iface.Methods {
 			methogString := "?" + method.Name
 			argString := ParseArguments(method.Args)
-			desc := append(desc, methogString+argString)
+			desc = append(desc, C.CString(methogString+argString))
 		}
 
-		for signal := range(iface.Signals) {
+		for _, signal := range iface.Signals {
 			signalString := "!" + signal.Name
 			argString := ParseArguments(signal.Args)
-			desc := append(desc, signalString+argString)
+			desc = append(desc, C.CString(signalString+argString))
 		}
 
-		for prop := range(iface.Properties) {
+		for _, prop := range iface.Properties {
 			propString := "@" + ParseArgumentOrProperty(prop.Name, prop.Access, prop.Type)
-			desc := append(desc, propString)
+			desc = append(desc, C.CString(propString))
 		}
 
-		
+		desc = append(desc, nil)
+		res = append(res, (C.AJ_InterfaceDescription)(&desc[0]))
 	}
+	return append(res, nil)
 }
 
 func ParseAllJoynObject(service *introspect.Node) C.AJ_Object {
-	obj := C.AJ_Object{C.CString, service.Name, ParseAllJoynInterfaces(service.Interfaces), 0, nil}
+	// Because of C struct alignment, we can't initialize inline and had to create accessor function
+	obj := C.Create_AJ_Object(C.CString(service.Name), &ParseAllJoynInterfaces(service.Interfaces)[0], C.uint8_t(0), unsafe.Pointer(nil))
 	return obj
 }
 
-func GetAllJoynObject(services []*introspect.Node) []C.AJ_Object {
-	res = make([]C.AJ_Object)
+func GetAllJoynObjects(services []*introspect.Node) []C.AJ_Object {
+	res := make([]C.AJ_Object, 0)
 
-	for service := range(services) {
-		res := append(res, parseAllJoynObject(service))
+	for _, service := range services {
+		res = append(res, ParseAllJoynObject(service))
 	}
 
 	return res
 }
 
 func (a *AllJoynBridge) StartAllJoyn(dbusService string) *dbus.Error {
-	
+	objects := GetAllJoynObjects(a.services[dbusService])
+	go func() {
+		C.AJ_Initialize()
+		C.AJ_PrintXML(&objects[0])
+		C.AJ_RegisterObjects(&objects[0], nil)
+		connected := false
+		var status C.AJ_Status = C.AJ_OK
+		for {
+			var msg C.AJ_Message
+			var busAttachment C.AJ_BusAttachment
+
+			if !connected {
+				status = C.AJ_StartService(&busAttachment,
+					nil,
+					60*1000, // TODO: Move connection timeout to config
+					C.FALSE,
+					25, // TODO: Move port to config
+					C.CString(dbusService),
+					C.AJ_NAME_REQ_DO_NOT_QUEUE,
+					nil)
+
+				if status != C.AJ_OK {
+					continue
+				}
+
+				log.Printf("StartService returned %d", status)
+				connected = true
+			}
+
+			status = C.AJ_UnmarshalMsg(&busAttachment,
+				&msg,
+				5*1000) // TODO: Move unmarshal timeout to config
+
+			if C.AJ_ERR_TIMEOUT == status {
+				continue
+			}
+
+			if C.AJ_OK == status {
+				switch msg.msgId {
+				case C.AJ_METHOD_ACCEPT_SESSION:
+					{
+						// uint16_t port;
+						// char* joiner;
+						// uint32_t sessionId;
+
+						// AJ_UnmarshalArgs(&msg, "qus", &port, &sessionId, &joiner);
+						status = C.AJ_BusReplyAcceptSession(&msg, C.TRUE)
+						log.Printf("ACCEPT_SESSION: %+v", msg)
+					}
+
+					// If it's a message for the app
+					// TODO: parse individual service, interace and method IDs and dispatch them to dbus
+				case (msg.msgId & 0x01000000):
+					log.Printf("Received application alljoyn message: %+v", msg)
+
+				case C.AJ_SIGNAL_SESSION_LOST_WITH_REASON:
+					{
+						// uint32_t id, reason;
+						// AJ_UnmarshalArgs(&msg, "uu", &id, &reason);
+						// AJ_AlwaysPrintf(("Session lost. ID = %u, reason = %u", id, reason));
+						log.Printf("Session lost: %+v", msg)
+					}
+
+				default:
+					/* Pass to the built-in handlers. */
+					status = C.AJ_BusHandleBusMessage(&msg)
+				}
+			}
+
+			/* Messages MUST be discarded to free resources. */
+			C.AJ_CloseMsg(&msg)
+
+			if status == C.AJ_ERR_READ {
+				C.AJ_Disconnect(&busAttachment)
+				log.Print("AllJoyn disconnected, retrying")
+				connected = false
+				C.AJ_Sleep(1000 * 2) // TODO: Move sleep time to const
+			}
+		}
+	}()
+	return nil
 }
 
-func (a *AllJoynBridge) addService(service string, node introspect.Node) {
-	services, ok = a.services[service]
+func (a *AllJoynBridge) addService(service string, node *introspect.Node) {
+	services, ok := a.services[service]
 	if ok {
 		a.services[service] = append(services, node)
 	} else {
-		a.services[service] = []*introspect.Node { node }
+		a.services[service] = []*introspect.Node{node}
 	}
 }
 
@@ -119,7 +203,7 @@ func (a *AllJoynBridge) AddService(dbusPath, dbusService, allJoynPath, allJoynIn
 		log.Printf("Error getting introspect from [%s, %s]: %s", dbusService, dbusPath, err)
 	}
 
-	addService(dbusService, node)
+	a.addService(dbusService, node)
 
 	log.Printf("Received introspect: %+v", node)
 
