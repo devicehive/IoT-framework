98ec12a9df9e7110528bf3963bc8c734b9a7d852
Sergey Demyanov <s.demyanov@gmail.com>
2015-07-08 02:50:20 -0400

    added gopkg.in/yaml.v2 dependency

--- build/snappy/devicehive-framework/build.sh
+++ build/snappy/devicehive-framework/build.sh
@@ -11,6 +11,7 @@ GOOS=linux go build -o $DIR/bin/x86_64/devicehive-ble $DIR/../../../devicehive-b
 GOOS=linux GOARCH=arm GOARM=7 go build -o $DIR/bin/armhf/devicehive-ble $DIR/../../../devicehive-ble/devicehive-ble.go
 
 # Cloud
+go get gopkg.in/yaml.v2
 go get github.com/gorilla/websocket
 go get github.com/mibori/gopencils
 GOOS=linux go build -o $DIR/bin/x86_64/devicehive-cloud $DIR/../../../devicehive-cloud/devicehive-cloud.go $DIR/../../../devicehive-cloud/rest-serve.go $DIR/../../../devicehive-cloud/ws-serve.go
