7f6fbe09881cc4ae67fff0feed42e5f5ef4bec2c
Artyom Astafurov <artyom.astafurov@gmail.com>
2015-07-21 11:16:08 -0700

    Removed unnecessary command handler

--- examples/gpio/adc.py
+++ examples/gpio/adc.py
@@ -12,16 +12,6 @@ def get_cloud():
     return dbus.Interface(dbus.SystemBus().get_object("com.devicehive.cloud", '/com/devicehive/cloud'), "com.devicehive.cloud")
 
 cloud = get_cloud()
-def command_received(id, name, parameters):
-    print("Command Received id : %s, name : %s, parameters : %s" % (id, name, parameters))
-    try:
-        # TODO: use json.dumps instead
-        s = "{\"command\" : \"%s\", \"id\" : %s, \"parameters\": %s}" % (name, id, parameters)
-        print(s) 
-        cloud.SendNotification("echo", s, 0)
-        cloud.UpdateCommand(dbus.UInt32(id), "OK", s)
-    except dbus.DBusException as e:
-        print(e)
 
 def adc_changed(port, value):
     print("pin_value_changed port: {} value: {}".format(port, value))
@@ -34,7 +24,6 @@ def main():
     pin_in.init("500", dbus_interface='com.devicehive.gpio.GpioPin')
     pin_in.connect_to_signal("pin_value_changed", adc_changed)
     
-    cloud.connect_to_signal("CommandReceived", command_received)
     GObject.MainLoop().run()
     try:
         GObject.MainLoop().run()
