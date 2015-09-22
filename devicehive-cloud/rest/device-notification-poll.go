7c07bba492aad57650b03d3d0f0a8826f6bfb7a7
Anton Rudnev <miborishante@gmail.com>
2015-09-22 12:19:46 +0300

    polling notifications without gopencils

--- devicehive-cloud/rest/device-notification-poll.go
+++ devicehive-cloud/rest/device-notification-poll.go
@@ -1,10 +1,12 @@
 package rest
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"time"
 
-	"github.com/devicehive/IoT-framework/devicehive-cloud/gopencils"
 	"github.com/devicehive/IoT-framework/devicehive-cloud/param"
 	"github.com/devicehive/IoT-framework/devicehive-cloud/say"
 )
@@ -15,6 +17,16 @@ type DeviceNotificationResource struct {
 	Notification string      `json:"notification"`
 	Parameters   interface{} `json:"parameters"`
 }
+
+/*
+
+Ubuntu 14.0.4 LTE. File leak version:
+
+2015/09/22 11:55:14 INFO:Polling notifications error: Get http://nn8615.pg.devicehive.com/api/device/0B24431A-EC99-4887-8B4F-38C3CEAF1D03/notification/poll: dial tcp: lookup nn8615.pg.devicehive.com: too many open files
+2015/09/22 11:55:15 INFO:Polling notifications error: Get http://nn8615.pg.devicehive.com/api/device/0B24431A-EC99-4887-8B4F-38C3CEAF1D03/notification/poll: dial tcp: lookup nn8615.pg.devicehive.com: too many open files
+2015/09/22 11:55:16 INFO:Polling notifications error: Get http://nn8615.pg.devicehive.com/api/device/0B24431A-EC99-4887-8B4F-38C3CEAF1D03/notification/poll: dial tcp: lookup nn8615.pg.devicehive.com: too many open files
+
+
 
 func DeviceNotificationPoll(
 	deviceHiveURL, deviceGuid, accessKey string,
@@ -45,6 +57,47 @@ func DeviceNotificationPoll(
 	if err == nil {
 		err = resource.ProcessedError()
 	}
+	return
+}
+
+*/
+
+func DeviceNotificationPoll(
+	deviceHiveURL, deviceGuid, accessKey string,
+	params []param.I, //maybe nil
+	client *http.Client, //maybe nil
+	requestOut chan *http.Request, //maybe nil
+) (dnrs []DeviceNotificationResource, err error) {
+	url := fmt.Sprintf("%s/device/%s/notification/poll", deviceHiveURL, deviceGuid)
+	if client == nil {
+		client = http.DefaultClient
+	}
+
+	//TODO: integrate params
+
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return
+	}
+	request.Header.Set("Authorization", "Bearer "+accessKey)
+	requestOut <- request
+
+	response, err := client.Do(request)
+	if err != nil {
+		return
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(body, &dnrs)
+	if err != nil {
+		return
+	}
+
 	return
 }
 
