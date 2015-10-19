19b590aa42cdad42ab6a871289863ab55e433015
Sergey Demyanov <s.demyanov@gmail.com>
2015-10-19 13:10:28 -0400

    fixed examples build

--- build/snappy/canonical-heart-pulse-demo/build.sh
+++ build/snappy/canonical-heart-pulse-demo/build.sh
@@ -3,5 +3,6 @@
 DIR="$(dirname "$(readlink -f "$0")")"
 
 go get github.com/godbus/dbus
+
 GOOS=linux go build -o $DIR/bin/x86_64/heart-pulse-demo $DIR/../../../examples/heart-pulse-demo.go
 GOOS=linux GOARCH=arm GOARM=7 go build -o $DIR/bin/armhf/heart-pulse-demo $DIR/../../../examples/heart-pulse-demo.go
--- build/snappy/sysmon/build.sh
+++ build/snappy/sysmon/build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-
 DIR="$(dirname "$(readlink -f "$0")")"
 
 go get github.com/shirou/gopsutil/cpu
 go get github.com/godbus/dbus
-GOOS=linux go build -o $DIR/bin/x86_64/sysmon $DIR/../../../examples/cpu-stats.go
-GOOS=linux GOARCH=arm GOARM=7 go build -o $DIR/bin/armhf/sysmon $DIR/../../../examples/cpu-stats.go
+GOOS=linux go build -o $DIR/bin/x86_64/sysmon $DIR/../../../examples/cpu-stats/cpu-stats.go
+GOOS=linux GOARCH=arm GOARM=7 go build -o $DIR/bin/armhf/sysmon $DIR/../../../examples/cpu-stats/cpu-stats.go
